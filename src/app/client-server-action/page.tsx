53fcb0f0ba629c5da0e04cb2adbd5e7305ef037e
faiz-alhadiid <faiz.alhadiid32@gmail.com>
2024-09-14 20:23:59 +0700

    test storing into local storage

--- src/app/client-server-action/page.tsx
+++ src/app/client-server-action/page.tsx
@@ -6,18 +6,24 @@ import { getCookie } from '@/app/actions/cookie';
 import { random } from '@/app/actions/action';
 
 export default function Page() {
-  const [ cookieValue, setCookieValue] = useState("");
+  const [cookieValue, setCookieValue] = useState("");
 
   useEffect(() => {
     // NextJS magic, getCookie will be called as server action and will be converted into RPC call
-    getCookie("CookieVar").then(x => setCookieValue(x?.value || ""));
+    getCookie("CookieVar").then(x => {
+      setCookieValue(x?.value || "");
+      localStorage.setItem("CookieVar", x?.value || "");
+    });
   }, []);
 
   return <div>
-    <div>Cooke Value: { cookieValue }</div>
+    <div>Cooke Value: {cookieValue}</div>
     <button onClick={async () => {
       const val = await random(); // NextJS magic
-      console.log("In client "+val);
-    }}>Click Me</button>
+      console.log("In client " + val);
+    }}>Random</button>
+    <button onClick={() => {
+      console.log(localStorage.getItem("CookieVar"))
+    }}>Local Storage</button>
   </div>;
 }
