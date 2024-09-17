45e079ea219542b173ab43aa14171f64b4dfedf7
faiz-alhadiid <faiz.alhadiid32@gmail.com>
2024-09-17 09:38:19 +0700

    update container

--- src/ui/SomeContainer.tsx
+++ src/ui/SomeContainer.tsx
@@ -2,10 +2,17 @@
 
 import { useEffect } from "react";
 
-export default function SomeContainer(props: {cookieValue?: string}){
-  useEffect(() => console.log("Loaded"), []);
+export default function SomeContainer(props: { cookieValue?: string }) {
+  useEffect(() => {
+    console.log("Loaded")
+    localStorage.setItem("CookieVar", props.cookieValue || "")
+  }, []);
+
   return <div>
     <h1>Sample Component</h1>
-    <div>Cooke Value: { props.cookieValue }</div>
+    <div>Cooke Value: {props.cookieValue}</div>
+    <button onClick={() => {
+      console.log(localStorage.getItem("CookieVar"))
+    }}>Local Storage</button>
   </div>;
 }
